9dfaca1bd2b305846aa1abec4972d092ea5f89de
LI-RENXIANG\lrx0014 <lrx0014@hotmail.com>
2018-03-06 20:18:35 +0800

    Implement navigation bar and basic views

---
+++ RelationshipCalculator/RelationshipCalculator/View/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using RelationshipCalculator.View;
+using RelationshipCalculator.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace RelationshipCalculator
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // 为不同的菜单创建不同的List类型
+        private List<NavMenuItem> navMenuPrimaryItem = new List<NavMenuItem>(
+            new[]
+            {
+                new NavMenuItem()
+                {
+                    FontFamily = new FontFamily("Segoe MDL2 Assets"),
+                    Icon = "\xE10F",
+                    Label = "称谓计算",
+                    Selected = Visibility.Visible,
+                    DestPage = typeof(CalculatorView)
+                },
+
+                new NavMenuItem()
+                {
+                    FontFamily = new FontFamily("Segoe MDL2 Assets"),
+                    Icon = "\xE11A",
+                    Label = "关系查询",
+                    Selected = Visibility.Collapsed,
+                    DestPage = typeof(SearchView)
+                },
+
+                new NavMenuItem()
+                {
+                    FontFamily = new FontFamily("Segoe MDL2 Assets"),
+                    Icon = "\xE897",
+                    Label = "帮助说明",
+                    Selected = Visibility.Collapsed,
+                    DestPage = typeof(AboutView)
+                },
+
+            });
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            // 绑定导航菜单
+            NavMenuPrimaryListView.ItemsSource = navMenuPrimaryItem;
+
+            // SplitView 开关
+            PaneOpenButton.Click += (sender, args) =>
+            {
+                RootSplitView.IsPaneOpen = !RootSplitView.IsPaneOpen;
+            };
+            // 导航事件
+            NavMenuPrimaryListView.ItemClick += NavMenuListView_ItemClick;
+
+            // 默认页
+            RootFrame.SourcePageType = typeof(CalculatorView);
+        }
+
+        private void NavMenuListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // 遍历，将选中Rectangle隐藏
+            foreach (var np in navMenuPrimaryItem)
+            {
+                np.Selected = Visibility.Collapsed;
+            }
+
+            NavMenuItem item = e.ClickedItem as NavMenuItem;
+            // Rectangle显示并导航
+            item.Selected = Visibility.Visible;
+            if (item.DestPage != null)
+            {
+                RootFrame.Navigate(item.DestPage);
+            }
+
+            RootSplitView.IsPaneOpen = false;
+        }
+    }
+}
